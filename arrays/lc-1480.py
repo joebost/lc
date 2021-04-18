2bff97793124e8f87fff2734f2023c78410e290e
joebost <joebost090@gmail.com>
2021-04-17 19:52:43 -0700

    init

---
+++ arrays/lc-1480.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+      sumList = [nums[0]]
+      for i in range(1, len(nums)):
+        sumList.append(sumList[i-1] + nums[i])
+      return sumList
---
+++ template.py
@@ -0,0 +1,10 @@
+class Solution:
+  def method(self, input):
+    return input
+
+def main():
+  solution = Solution()
+  print(solution.method("test"))
+
+if __name__ == "__main__":
+  main()
